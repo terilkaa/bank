d9b1688c3de84c918c2d7f034db4932e1b3dbfaf
terilkaa <terezka.sulc@gmail.com>
2023-11-16 01:14:00 +0100

    Update Bank.java

--- src/main/java/org/bank/Bank.java
+++ src/main/java/org/bank/Bank.java
@@ -2,20 +2,25 @@ package org.bank;
 
 import com.google.inject.Inject;
 import org.bank.account.*;
+import org.bank.factories.BaseAccJsonSerialFactory;
+import org.bank.factories.OwnerJsonSerialFactory;
+import org.bank.factories.SavingAccJsonSerialFactory;
+import org.bank.factories.StudentAccJsonSerialFactory;
+import org.bank.file.FileService;
 import org.bank.json.IJsonGenerator;
 import org.bank.json.JsonGson;
-import org.bank.json.JsonLog;
+import org.bank.json.JsonLoadService;
+import org.bank.json.objects.BaseAccJsonSerialObj;
+import org.bank.json.objects.OwnerJsonSerialObj;
+import org.bank.json.objects.SavingAccJsonSerialObj;
+import org.bank.json.objects.StudentAccJsonSerialObj;
 import org.bank.moneyTransfer.InterestTransfer;
 import org.bank.moneyTransfer.Transfer;
 import org.bank.people.Owner;
-import org.bank.people.OwnerFactory;
 import org.bank.people.OwnerService;
-import org.bank.print.ConsoleLog;
 import org.bank.print.ILogger;
-import org.bank.print.ServiceLog;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,29 +37,59 @@ public class Bank {
     InterestTransfer interestTransfer;
     @Inject
     IJsonGenerator iJsonGenerator;
+    @Inject
+    BaseAccJsonSerialFactory baseAccJsonSerialFactory;
+    @Inject
+    StudentAccJsonSerialFactory studentAccJsonSerialFactory;
+    @Inject
+    SavingAccJsonSerialFactory savingAccJsonSerialFactory;
+    @Inject
+    OwnerJsonSerialFactory ownerJsonSerialFactory;
+    @Inject
+    FileService fileService;
+    @Inject
+    JsonLoadService jsonLoadService;
     public void run() throws Exception {
 
-        BaseAcc baseAcc = accountService.createStoreBaseAcc(ownerService.createOwner("noejf", "fnuewo"), 2000);
-        StudentAcc studentAcc = accountService.createStoreStudentAcc(ownerService.createOwner("noejf", "fnuo"), 2000);
-        iLogger.accNumLog(studentAcc);
-        StudentAcc studentAcc1 = accountService.createStoreStudentAcc(ownerService.createOwner("njf", "fnuewo"), 2000);
-        iLogger.accNumLog(studentAcc1);
-
+        List<StudentAccJsonSerialObj> studentAccJsonSerialObjs= new ArrayList<StudentAccJsonSerialObj>();
+        List<SavingAccJsonSerialObj> savingAccJsonSerialObjs= new ArrayList<SavingAccJsonSerialObj>();
+        List<BaseAccJsonSerialObj> baseAccJsonSerialObjs= new ArrayList<BaseAccJsonSerialObj>();
+        List<OwnerJsonSerialObj> ownerJsonSerialObjs= new ArrayList<OwnerJsonSerialObj>();
+        if(fileService.doesFileExist("data_json/Student.json")&& fileService.doesFileExist("data_json/Saving.json") &&fileService.doesFileExist("data_json/Base.json") && fileService.doesFileExist("data_json/Owner.json"))
+        {
+            jsonLoadService.loadJson(fileService.readFile("data_json/Student.json"), fileService.readFile("data_json/Saving.json"), fileService.readFile("data_json/Base.json"), fileService.readFile("data_json/Owner.json"));
+        } else {
+            accountService.createStoreBaseAcc(ownerService.createOwner("fiaho","fewo"), 452);
+            accountService.createStoreStudentAcc(ownerService.createOwner("wi","fihwo"), 784512);
+            accountService.createStoreSavingAcc(ownerService.createOwner("fisfcweaho","wfew"), 9461);
+        }
 
         for (Map.Entry<String, BaseAcc> entrySet : accountService.getAccounts().entrySet()) {
             BaseAcc acc = entrySet.getValue();
-
-            iLogger.balanceLog(acc);
             if (acc instanceof InterestAcc) {
-                iLogger.balanceLog(acc);
                 interestTransfer.InterestAdd(acc);
-                iLogger.balanceLog(acc);
             }
+            if(acc instanceof StudentAcc) {
+                studentAccJsonSerialObjs.add(studentAccJsonSerialFactory.createStudentAcc((StudentAcc) acc));
+            } else if(acc instanceof SavingAcc){
+                savingAccJsonSerialObjs.add(savingAccJsonSerialFactory.createSavingAcc((SavingAcc) acc));
+            } else {
+                baseAccJsonSerialObjs.add(baseAccJsonSerialFactory.createBaseAcc(acc));
+            }
+                    }
+        for (Map.Entry<Integer, Owner> entrySet : ownerService.getOwners().entrySet()) {
+            Owner owner = entrySet.getValue();
+            ownerJsonSerialObjs.add(ownerJsonSerialFactory.createJSONOwner(owner));
         }
-        iLogger.debug(iJsonGenerator.generateJSONString(ownerService.createOwner("ncfwo", "jfiewo")));
-        iLogger.debug(iJsonGenerator.generateJSONString(ownerService.createOwner("jiewp","fnwo")));
+        for (Map.Entry<String, BaseAcc> entrySet : accountService.getAccounts().entrySet()) {
+            BaseAcc acc = entrySet.getValue();
+            iLogger.balanceLog(acc);
+        }
 
-        iLogger.debug(iJsonGenerator.generateJSONString(ownerService.createOwner("jiewp","fnwo")));
+        fileService.writeFile("data_json/Student.json",iJsonGenerator.generateJSONString(studentAccJsonSerialObjs));
+        fileService.writeFile("data_json/Base.json",iJsonGenerator.generateJSONString(baseAccJsonSerialObjs));
+        fileService.writeFile("data_json/Saving.json",iJsonGenerator.generateJSONString(savingAccJsonSerialObjs));
+        fileService.writeFile("data_json/Owner.json",iJsonGenerator.generateJSONString(ownerJsonSerialObjs));
     }
 }
 
