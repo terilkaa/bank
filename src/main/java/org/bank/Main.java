b35cc72e1be614aa2bacb63692774ba4edd641e7
terilkaa <terezka.sulc@gmail.com>
2023-11-22 23:03:03 +0100

    add eventbus

--- src/main/java/org/bank/Main.java
+++ src/main/java/org/bank/Main.java
@@ -2,6 +2,9 @@ package org.bank;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.bank.notification.NotifyCustomerEvent;
+import org.bank.notification.NotifyCustomerEventListener;
+import org.bank.notification.NotifyNewsletterEventListener;
 import org.bank.validation.BankInjector;
 
 public class Main {
@@ -9,6 +12,8 @@ public class Main {
         try {
             Injector injector = Guice.createInjector(new BankInjector());
             Bank bank = injector.getInstance(Bank.class);
+            injector.getInstance(NotifyCustomerEventListener.class);
+            injector.getInstance(NotifyNewsletterEventListener.class);
             bank.run();
         }
         catch(Exception e)
--- src/main/java/org/bank/validation/BankInjector.java
+++ src/main/java/org/bank/validation/BankInjector.java
@@ -1,15 +1,32 @@
 package org.bank.validation;
 
+import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.InjectionListener;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 import org.bank.json.IJsonGenerator;
 import org.bank.json.JsonGson;
 import org.bank.print.ConsoleLog;
 import org.bank.print.ILogger;
 
 public class BankInjector extends AbstractModule {
+    private final EventBus eventBus = new EventBus();
     protected void configure()
     {
         this.bind(ILogger.class).to(ConsoleLog.class);
         this.bind(IJsonGenerator.class).to(JsonGson.class);
+        this.bind(EventBus.class).toInstance(eventBus);
+        bindListener(Matchers.any(), new TypeListener() {
+            public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
+                encounter.register(new InjectionListener<I>(){
+                    public void afterInjection(I i) {
+                        eventBus.register(i);
+                    }
+                });
+            }
+        });
     }
 }
