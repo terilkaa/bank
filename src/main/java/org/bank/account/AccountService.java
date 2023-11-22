e24e273deb4055d09ad5f0a96ae629af8ccf8cc4
terilkaa <terezka.sulc@gmail.com>
2023-11-22 23:03:35 +0100

    add eventbus to AccountService

--- src/main/java/org/bank/account/AccountService.java
+++ src/main/java/org/bank/account/AccountService.java
@@ -3,11 +3,12 @@ package org.bank.account;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.bank.factories.BaseAccFactory;
-import org.bank.factories.SavingAccFactory;
-import org.bank.factories.StudentAccFactory;
+import jdk.jfr.Event;
+import org.bank.factories.*;
+import org.bank.notification.NotifyCustomerEvent;
 import org.bank.people.Owner;
 @Singleton
 public class AccountService {
@@ -17,7 +18,12 @@ public class AccountService {
     private SavingAccFactory savingAccFactory;
     @Inject
     private StudentAccFactory studentAccFactory;
-
+    @Inject
+    private NotificationDataFactory notificationDataFactory;
+    @Inject
+    private NotifyCustomerEventFactory notifyCustomerEventFactory;
+    @Inject
+    private EventBus eventBus;
     private Map<String, BaseAcc> accounts;
 
     public AccountService() {
@@ -27,18 +33,25 @@ public class AccountService {
     public BaseAcc createStoreBaseAcc(Owner owner, double balance) {
         BaseAcc baseAcc = this.baseAccFactory.createBaseAcc(owner, balance);
         this.accounts.put(baseAcc.getAccountNum(), baseAcc);
+        eventBus.post(createEvent(owner.getFullName()));
         return baseAcc;
     }
     public StudentAcc createStoreStudentAcc(Owner owner, double balance) {
         StudentAcc studentAcc = studentAccFactory.createStudentAcc(owner, balance);
         this.accounts.put(studentAcc.getAccountNum(), studentAcc);
+        eventBus.post(createEvent(owner.getFullName()));
         return studentAcc;
     }
     public SavingAcc createStoreSavingAcc(Owner owner, double balance) {
         SavingAcc savingAcc = savingAccFactory.createSavingAcc(owner, balance);
         this.accounts.put(savingAcc.getAccountNum(), savingAcc);
+        eventBus.post(createEvent(owner.getFullName()));
         return savingAcc;
     }
+    private NotifyCustomerEvent createEvent(String customerName){
+        return notifyCustomerEventFactory.createNotifyCustomerEvent(notificationDataFactory.createNotificationData(customerName));
+    }
+
     public Map<String, BaseAcc> getAccounts() {
         return accounts;
     }
